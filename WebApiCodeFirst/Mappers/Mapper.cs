0fdb5456713ff59dd3afc77eeae93691392c9901
roque <roque@DESKTOP-5F04UKI>
2025-09-01 18:50:24 -0300

    Agregar archivos de proyecto.

---
+++ WebApiCodeFirst/Mappers/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebApiCodeFirst.Models;
+using WebApiCodeFirst.Models.DTO;
+
+namespace WebApiCodeFirst.Mappers
+{
+    public class Mapper : Profile
+    {
+        protected Mapper()
+        {
+            CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+        }
+    }
+}
